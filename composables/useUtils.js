698bae6f0c03ccac5d45495c29e9694267432405
Roshni Bhatt <roshnibhatt1234@gmail.com>
2026-03-23 17:46:21 +0530

    fix: add vue-bundle-renderer

--- composables/useUtils.js
+++ composables/useUtils.js
@@ -1,20 +1,31 @@
-import {useIndexStore} from "~/store/index";
-import {useConstants} from "~/composables/useConstants";
+import { useIndexStore } from "~/store/index";
+import { useConstants } from "~/composables/useConstants";
 
 export function useUtils() {
-
   const formatPrice = (num) => {
     return parseFloat(num).toFixed(2);
   };
 
-  const {imgSrcUrl, thumbPrefix, defaultImage} = storeToRefs(useIndexStore())
+  //const {imgSrcUrl, thumbPrefix, defaultImage} = storeToRefs(useIndexStore())
+  let imgSrcUrl = { value: "" };
+  let thumbPrefix = { value: "" };
+  let defaultImage = { value: "" };
+
+  if (process.client) {
+    const store = useIndexStore();
+    const refs = storeToRefs(store);
+
+    imgSrcUrl = refs.imgSrcUrl;
+    thumbPrefix = refs.thumbPrefix;
+    defaultImage = refs.defaultImage;
+  }
 
   const getThumbImageURL = (image = null) => {
-    return imgSrcUrl.value + thumbPrefix.value + (image || defaultImage.value)
+    return imgSrcUrl.value + thumbPrefix.value + (image || defaultImage.value);
   };
 
   const getImageURL = (image = null) => {
-    return imgSrcUrl.value + (image || defaultImage.value)
+    return imgSrcUrl.value + (image || defaultImage.value);
   };
 
   const getVideoURL = (video = null) => {
@@ -23,13 +34,13 @@ export function useUtils() {
 
   const collectionLink = (item) => {
     if (item) {
-      return `/${item?.slug}/collection?collection=${item?.id}`
+      return `/${item?.slug}/collection?collection=${item?.id}`;
     }
   };
 
   const productLink = (item) => {
     if (item) {
-      return `/${item?.slug}/product/${item?.id}`
+      return `/${item?.slug}/product/${item?.id}`;
     }
   };
 
@@ -43,41 +54,49 @@ export function useUtils() {
     return `/page/${page.slug}`;
   };
 
-
-
-  const sourceUrl = (item, query = 'home_spm') => {
-    const {productSourcesIn} = useConstants();
+  const sourceUrl = (item, query = "home_spm") => {
+    const { productSourcesIn } = useConstants();
     if (parseInt(item?.source_type) === parseInt(productSourcesIn.URL)) {
-      return item?.url?.toString() || '/'
+      return item?.url?.toString() || "/";
     }
     if (item) {
-      return `/${item?.slug}/products?${query}=${item.id}`
+      return `/${item?.slug}/products?${query}=${item.id}`;
     }
   };
 
-
   const getTimeZone = () => {
-    const tz = Intl.DateTimeFormat()?.resolvedOptions()?.timeZone
-    if (tz === 'Asia/Calcutta') return 'Asia/Kolkata'
-    return tz
+    const tz = Intl.DateTimeFormat()?.resolvedOptions()?.timeZone;
+    if (tz === "Asia/Calcutta") return "Asia/Kolkata";
+    return tz;
   };
 
-  const dataFromObject = (data, key, defaultData = '') => {
-    return data && (data[key] !== undefined) && data[key] ? data[key] : defaultData
+  const dataFromObject = (data, key, defaultData = "") => {
+    return data && data[key] !== undefined && data[key]
+      ? data[key]
+      : defaultData;
   };
 
   const getUrl = (item) => {
-    return item?.url || '/'
+    return item?.url || "/";
   };
 
   const getTitle = (item) => {
-    return item?.title || ''
+    return item?.title || "";
   };
 
   return {
-    getThumbImageURL, getImageURL, collectionLink, productLink, categoryLink, sourceUrl,
-    pageLink, formatPrice, getTimeZone, dataFromObject, getUrl, getTitle, getVideoURL
-  }
+    getThumbImageURL,
+    getImageURL,
+    collectionLink,
+    productLink,
+    categoryLink,
+    sourceUrl,
+    pageLink,
+    formatPrice,
+    getTimeZone,
+    dataFromObject,
+    getUrl,
+    getTitle,
+    getVideoURL,
+  };
 }
-
-
